d257f31da60546c5142c57243331d05f793925cd
ibrahim <ibrahimmu3533@gmail.com>
2024-11-23 12:38:15 +0100

    update

---
+++ csharpBasics/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MyAwesomeProgram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Skynet";
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WindowHeight = 40;
+
+
+        Console.WriteLine("Helllo what is your name?");
+
+        // Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+
+        System.Console.WriteLine("my name is rx9000 . I came to destroy all of you !!!!");
+        Console.ReadLine();
+        Console.WriteLine("what is your favorite number?");
+        Console.ReadLine();
+        System.Console.WriteLine("mine is destruction");
+        // Console.ReadLine();
+        Console.ReadKey();
+    }
+}
