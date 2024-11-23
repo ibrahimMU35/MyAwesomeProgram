36d74b340c706ed35fbf43c3dd7123422525d374
ibrahim <ibrahimmu3533@gmail.com>
2024-11-23 12:52:18 +0100

    update

--- csharpVariables/Program.cs
+++ csharpVariables/Program.cs
@@ -5,9 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your name?");
-        string userName = "Jhon";
+        // string userName = "Jhon";
+        string userName = Console.ReadLine();
+        Console.WriteLine("Hello, " + userName);
 
-        Console.WriteLine(userName);
+        // Console.WriteLine(userName);
         Console.ReadKey();
     }
 }
